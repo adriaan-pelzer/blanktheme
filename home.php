4bfa9b0d6675a0e068b6e59ac50c2ada837f14bc
Adriaan Pelzer <adriaan@wewillraakyou.com>
2010-11-08 16:12:37 +0000

    Import

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: Home
+*/
+?>
+<?php get_header (); ?>
+<section id="homebody" class="body">
+</section>
+<?php get_footer (); ?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php get_header (); ?>
+<section>
+</section>
+<?php get_footer (); ?>
