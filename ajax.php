c3e5925abb21155937b101f8a7a81c6745d782f4
Adriaan Pelzer <adriaan@wewillraakyou.com>
2010-11-09 17:02:10 +0000

    Add error checking to ajax.php

--- ajax.php
+++ ajax.php
@@ -9,7 +9,7 @@ function return_json ($json) {
 
 if (!($wp_config = file_get_contents ("../../../wp-config.php"))) {
     $error = "Cannot read wp-config.php";
-    json_decode (array ('code'=>-1, 'error'=>$error));
+    return_json (array ('code'=>-1, 'error'=>$error));
 }
 
 $db_name = array();
@@ -20,38 +20,38 @@ $db_pref = array();
 
 if (!(preg_match ("/.*DB_NAME\', \'(.*)\'.*/", $wp_config, $db_name))) {
     $error = "Cannot find DB_NAME";
-    json_decode (array ('code'=>-1, 'error'=>$error));
+    return_json (array ('code'=>-1, 'error'=>$error));
 }
 
 if (!(preg_match ("/.*DB_HOST\', \'(.*)\'.*/", $wp_config, $db_host))) {
     $error = "Cannot find DB_HOST";
-    json_decode (array ('code'=>-1, 'error'=>$error));
+    return_json (array ('code'=>-1, 'error'=>$error));
 }
 
 if (!(preg_match ("/.*DB_USER\', \'(.*)\'.*/", $wp_config, $db_user))) {
     $error = "Cannot find DB_USER";
-    json_decode (array ('code'=>-1, 'error'=>$error));
+    return_json (array ('code'=>-1, 'error'=>$error));
 }
 
 if (!(preg_match ("/.*DB_PASSWORD\', \'(.*)\'.*/", $wp_config, $db_pass))) {
     $error = "Cannot find DB_PASSWORD";
-    json_decode (array ('code'=>-1, 'error'=>$error));
+    return_json (array ('code'=>-1, 'error'=>$error));
 }
 
 if (!(preg_match ("/.*table_prefix[^\']+\'([^\']+)\'.*/", $wp_config, $db_pref))) {
     $error = "Cannot find table_prefix";
-    json_decode (array ('code'=>-1, 'error'=>$error));
+    return_json (array ('code'=>-1, 'error'=>$error));
 }
 
 if (!($mysql = mysql_connect ($db_host[1], $db_user[1], $db_pass[1]))) {
     $error = "Cannot connect to database: ".mysql_error ();
-    json_decode (array ('code'=>-1, 'error'=>$error));
+    return_json (array ('code'=>-1, 'error'=>$error));
 }
 
 if (!(mysql_select_db ($db_name[1], $mysql))) {
     $error = "Cannot select database: ".mysql_error ();
-    json_decode (array ('code'=>-1, 'error'=>$error));
+    return_json (array ('code'=>-1, 'error'=>$error));
 }
 
-json_decode (array ('code'=>-1, 'error'=>'Unimplemented'));
+return_json (array ('code'=>-1, 'error'=>'Unimplemented'));
 ?>
